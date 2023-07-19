a20d40a07d7df078252f3639ba21c5305e133c3d
gitadityakumar <iamadityakumar05@proton.me>
2023-07-19 19:01:57 +0530

    first program

---
+++ app.js
@@ -0,0 +1,10 @@
+const amount=12
+if(amount<14){
+    console.log('small number');
+}else{
+    console.log('big number');
+}
+
+console.log(`hey it's my first node app`);
+console.log(__dirname);
+console.log(__filename);
