a2a76e0bb8621b85a36cba234580522f2f93bb02
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-08-19 22:34:01 +0530

    half way of making a web server using Expressjs

---
+++ expressjsWebServer/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get(`/`,(req,res)=>{
+    // res.sendFile('./views/index.html', {root: __dirname});
+        // res.send('Hello World');
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
